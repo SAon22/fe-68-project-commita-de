ea282627f3ccdf6301b037552e36b88113e41fae
Siraphop <siraphop2707@gmail.com>
2026-03-21 22:03:06 +0700

    fix data in interface match with db

--- interface.ts
+++ interface.ts
@@ -1,4 +1,24 @@
+interface MassageShopItem {
+    _id: string,
+    name: string,
+    address: string,
+    district: string,
+    province: string,
+    postalcode: string,
+    tel: string,
+    picture: string,
+    openCloseTime: string,
+  }
+  
+  interface MassageShopJson {
+    success: boolean,
+    count: number,
+    pagination: Object,
+    data: MassageShopItem[]
+  }
+
 export interface ReservationItem {
+    name: string;
     shopName: string;
     tel: string;
     date: string;
