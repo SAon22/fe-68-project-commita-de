02ea42950968350f7b70ca029c0009bc5a27c401
Siraphop <siraphop2707@gmail.com>
2026-03-22 21:14:35 +0700

    get and delte

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -1,8 +1,12 @@
 import { dbConnect } from "@/db/dbConnect"
 import Reservation from "@/db/models/Reservation"
 import { NextResponse } from "next/server"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../auth/[...nextauth]/authOptions"
 
+// add reservation
 export async function POST(req: Request) {
+
     await dbConnect()
 
     const body = await req.json()
@@ -10,8 +14,49 @@ export async function POST(req: Request) {
     try {
         const reservation = await Reservation.create(body)
         return NextResponse.json(reservation)
-    } catch (err) {
+    } catch(err) {
         console.log(err);
-        return NextResponse.json({ error: "Cannot create reservation" }, { status: 500 })
+        return NextResponse.json({error:"Cannot create reservation"}, {status:500})
+    }
+}
+// show reservation
+export async function GET() {
+
+    await dbConnect()
+
+    const session = await getServerSession(authOptions)
+
+    if (!session) {
+        return NextResponse.json({message:"Unauthorized"}, {status:401})
+    }
+
+    let reservations
+
+    if(session.user.role === "admin") {
+        reservations = await Reservation.find().populate("massageShop")
+    } else {
+        reservations = await Reservation.find({user:session.user._id}).populate("massageShop")
+    }
+
+    return NextResponse.json(reservations)
+}
+// delete reservation
+export async function DELETE(req: Request) {
+
+    await dbConnect()
+
+    const { searchParams } = new URL(req.url)
+    const id = searchParams.get("id")
+
+    if(!id) {
+        return NextResponse.json({error:"Missing id"}, {status:400})
+    }
+
+    try {
+        await Reservation.findByIdAndDelete(id)
+        return NextResponse.json({message:"Deleted"})
+    } catch(err) {
+        console.log(err)
+        return NextResponse.json({error:"Delete failed"}, { status:500})
     }
 }
