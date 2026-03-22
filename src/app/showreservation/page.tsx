240a7b6e02e25836cc358b3c968f4dabf013a472
Siraphop <siraphop2707@gmail.com>
2026-03-22 21:12:39 +0700

    show user and admin case reservation

---
+++ src/app/showreservation/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import ReservationList from "@/components/ReservationList";
+
+export default function MyReservationPage() {
+    return (
+        <main className="p-5">
+            <h1 className="text-2xl font-bold text-center mb-5">My Reservations</h1>
+            <ReservationList/>
+        </main>
+    )
+}
