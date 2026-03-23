4b557f89baec7066104a07e8a5ced038b3385c5b
Siraphop <siraphop2707@gmail.com>
2026-03-23 15:09:19 +0700

    fix TopMeu show all page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner";
+import TopMenu from "@/components/TopMenu";
 
 export default function Home() {
   return (
     <main>
       <Banner/>
+      <TopMenu/>
     </main>
   );
 }
